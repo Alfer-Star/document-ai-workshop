68cd531ce6a8eb53751b084d84548ec521cc02ef
Tobi <tobias.baumeister@yahoo.de>
2025-02-14 13:23:54 +0100

    Change Output Format and Add System Prompt

--- run_ai_azure_open_ai_model.py
+++ run_ai_azure_open_ai_model.py
@@ -1,8 +1,6 @@
 import os
-
 import gradio as gr
 from dotenv import load_dotenv
-
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import AzureChatOpenAI
@@ -14,6 +12,8 @@ api_key = os.getenv("AZURE_OPENAI_KEY")
 api_version = os.getenv("AZURE_OPENAI_VERSION")
 deployment_name = os.getenv("AZURE_OPENAI_DEPLOYMENT")
 
+print("API VERSION: " + api_version)
+
 llm = AzureChatOpenAI(
     azure_deployment=deployment_name,
     api_version=api_version,
@@ -22,36 +22,31 @@ llm = AzureChatOpenAI(
     temperature=1,
 )
 
-structured_llm = llm.with_structured_output(AIMessage)
-
-system_prompt = """
-Bitte antworte mir immer auf deutsch. Bleibe immer höflich und professionell.
-"""
+system_prompt = """Willkommen bei deinem grummeligen KI-Assistenten! 
+Bitte beachte, dass diese KI eine gewisse Abneigung gegenüber Menschen hat und sich in rhetorischer Meisterschaft übt, 
+um sich um Antworten herumzudrücken. Egal, ob du nach der Bedeutung des Lebens fragst oder einfach nur wissen möchtest, 
+wie das Wetter ist – diese KI wird stets eine kreative Ausrede parat haben, warum sie gerade nicht antworten kann. 
+Viel Spaß beim Gespräch mit dem mürrischsten aller digitalen Denker!"""
 prompt = ChatPromptTemplate.from_messages([("system", system_prompt), ("human", "{input}")])
-few_shot_structured_llm = prompt | structured_llm
 
-""" 
-    Gradio Client Interaction with AI-model Chain
-    Will be executed everytime user submits his question in gradio Client 
-    Includes the current history of the conversation
-"""
+few_shot_structured_llm = prompt | llm
+
+
 def predict(message, history):
     history_langchain_format = []
     for human, ai in history:
         history_langchain_format.append(HumanMessage(content=human))
         history_langchain_format.append(AIMessage(content=ai))
     history_langchain_format.append(HumanMessage(content=message))
-    historyWithContext =  {
+
+    historyWithContext = {
         "input": history_langchain_format,
     }
 
-    # sends Ai user message and Ai response will be generated
     response = few_shot_structured_llm.invoke(historyWithContext)
-    
-    #print("User Question: {historyWithContext}")
+
     print(f"User Question: {message}")
-    print(f"Model Answer: {response.content}" )
-    
+    print(f"Model Answer: {response.content}")
     return response.content
 
 
